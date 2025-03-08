d13e9e3cb7113332f4bd53970459b1fb865dc37e
AminBadani <ahmadaminbadani@gmail.com>
2025-03-08 21:52:50 +0700

    membuat class ListKata, memindahkan function update_list ke dalam class ListKata, dan menambahkan algoritma dynamic programming

--- local/modules.py
+++ local/modules.py
@@ -7,17 +7,28 @@ def open_dataset(file_name: str):
     
     return words
 
+class ListKata:
+    def __init__(self, dataset):
+        self.dataset = dataset
+        self.cache = {}
+        self.list_lama = {}
+        self.input_lama = ''
 
-# Update list rekomendasi kata berdasarkan input pengguna
-def update_list(old_list, input_word):
-    # Cari kata yang awalanya sama dengan input
-    prefix_pattern = re.compile('^' + input_word) 
-    new_list = list();
+    def update_list(self, input_kata: str):
+        prefix = re.compile('^' + input_kata);
+        list_rekomendasi = {};
 
-    # Cek input dan masukkan ke dalam list jika pola-nya cocok
-    if (len(input_word) > 0): 
-        for word in old_list:
-            if (len(input_word) > len(word)): continue;
-            if (prefix_pattern.match(word)): new_list.append(word);
-    
-    return new_list
+        if ( input_kata in self.cache )                             : return self.cache[input_kata]
+        if ( len(input_kata) < len(self.input_lama) )               : self.list_lama = {}
+        if ( input_kata[:len(self.input_lama)] != self.input_lama ) : self.list_lama = {}
+
+        if (len(self.list_lama) == 0): 
+            list_rekomendasi = [word for word in self.dataset if prefix.match(word)]
+        else: 
+            list_rekomendasi = [word for word in self.list_lama if prefix.match(word)]
+
+        self.list_lama = list_rekomendasi
+        self.input_lama = input_kata
+        self.cache[input_kata] = list_rekomendasi;
+
+        return list_rekomendasi
