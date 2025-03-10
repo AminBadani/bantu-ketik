1a8d057bd5b373c6c035750087f7f646bb2f9d84
AminBadani <ahmadaminbadani@gmail.com>
2025-03-10 22:03:12 +0700

    menambahkan fitur ganti isi input_field berdasarkan kata yang dipilih

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import tkinter;
-from local.modules import open_dataset, update_list;
+from local.modules import open_dataset;
 from local.modules import ListKata;
 
 # Constant
@@ -21,8 +21,8 @@ input_word = tkinter.StringVar();
 
 
 # -- Functions
-# Mengecek jika terjadi perubahan pada input pengguna secara realtime
-def on_change(*args):
+# Dipanggil ketika terjadi perubahan pada input pengguna
+def input_on_change(*args):
     listbox_words.delete(0, 'end');
 
     new_list = dataset.update_list(input_word.get())
@@ -32,18 +32,39 @@ def on_change(*args):
 
     return True;
 
+# Dipanggil ketika listbox di klik menggunakan enter atau klik kanan pada mouse
+def listbox_on_select(event):
+    if (listbox_words.index('end') != 0):
+        # Mengambil index dari kata yang dipilih
+        for i in listbox_words.curselection():
+            selected = listbox_words.get(i)
+            # Menghapus lalu mengisikan input sesuai dengan kata yang dipilih
+            input_field.delete(0, 'end');
+            input_field.insert(0, selected)
+
+# Dipanggil ketika tombol pada keyboard ditekan
+def keyboard_on_press(event):
+    # Cek apakah pengguna menekan keyboard bawah atau escape
+    if (event.keysym == 'Down'):
+        listbox_words.focus_set();
+    elif (event.keysym == 'Escape'):
+        input_field.focus_set();
+
 
 # -- COMPONENTS
 # Konfigurasi title, window size, dan disable minimize-maximize
 app.title("BantuKetik - Simple Autocomplete");
 app.geometry(screen_resolution)
 app.resizable(False,False); 
+app.bind("<Key>", keyboard_on_press);
 
 # Input yang digunakan pengguna untuk mengetikkan kata
 input_field = tkinter.Entry(app, textvariable=input_word);
-input_word.trace_add(['write'], on_change);
+input_word.trace_add(['write'], input_on_change);
 # List kata yang ditampilakan ke pengguna
 listbox_words = tkinter.Listbox(app, height=HEIGHT,)
+listbox_words.bind("<Return>", listbox_on_select);
+listbox_words.bind("<Button>", listbox_on_select);
 # Konfigurasi tata letak
 input_field.pack(fill=tkinter.X, padx=(PADDING), pady=(PADDING, 0))
 listbox_words.pack(fill=tkinter.X, padx=(PADDING))
