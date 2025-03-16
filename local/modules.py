326904fecccd1de168aae8fba6cb1c87e427a7a1
AminBadani <ahmadaminbadani@gmail.com>
2025-03-16 10:59:56 +0700

    update alur dari function update_list

--- local/modules.py
+++ local/modules.py
@@ -3,55 +3,61 @@ import re;
 # Buka dan baca list kata yang ada di dalam dataset
 def open_dataset(file_name: str):
     with open(file_name, 'r') as dataset:
-        words = dataset.read().split('\n');
+        list_kata = dataset.read().split('\n');
     
-    return words
+    return list_kata
 
 # Class yang digunakan untuk algoritma string matching dan dynamic programming
 class ListKata:
     def __init__(self, dataset):
         self.dataset = dataset
-        # cache digunakan untuk menyimpan list kata dari input yang sudah pernah di ketik
-        self.cache = {}
+        # history digunakan untuk menyimpan list kata dari input yang sudah pernah di ketik
+        self.history = {}
         # list_lama digunakan untuk menyimpan list kata dari hasil rekomendasi kata sebelumnya
-        self.list_lama = {}
+        self.list_lama = []
         self.input_lama = ''
 
     def update_list(self, input_kata: str):
         # Masukkan input kata ke dalam regex
         prefix = re.compile('^' + input_kata, re.IGNORECASE);
-        list_rekomendasi = {};
+        list_rekomendasi = [];
 
         # Cek apakah input kata sudah pernah di ketik
-        # jika iya, maka ambil dari list kata dari cache
-        if (input_kata in self.cache): 
-            return self.cache[input_kata]
+        # jika iya, maka ambil dari list kata dari history
+        if (input_kata in self.history): 
+            self.list_lama = self.history[input_kata]
+            self.input_lama = input_kata
+            return self.history[input_kata]
         
-        '''
-        Cek apakah input kata saat ini kurang dari input kata lama
-        ini digunakan untuk mengecek jika user menghapus salah satu huruf dari input kata
-        '''
-        '''
-        Cek apakah n huruf pertama dari input kata tidak sama dengan input lama
-        ini digunakan untuk mengecek apakah pengguna menambahkan huruf baru bukan di akhir kata
-        '''
-        if (len(input_kata) < len(self.input_lama) or 
-                input_kata[:len(self.input_lama)] != self.input_lama): 
-            self.list_lama = {}
-
-        # Cek isi dari list_lama
-        # jika tidak ada isinya, maka filter kata dari dataset
-        # jika ada isinya, maka filter kata dari list_lama
+        # Cek apakah list_lama ada isinya
         if (len(self.list_lama) == 0): 
-            list_rekomendasi = [word for word in self.dataset if prefix.match(word)]
-        else: 
-            list_rekomendasi = [word for word in self.list_lama if prefix.match(word)]
+            # Cari melalui dataset
+            for kata in self.dataset:
+                    if prefix.match(kata):
+                        list_rekomendasi.append(kata)
 
-        # Masukkan hasil filter kata yang ada di dalam list_rekomendasi ke list_lama
+        else: 
+            # Cari melalui dataset
+            # Karena pengguna tidak menambahkan input di akhir string
+            # atau pengguna menghapus huruf dalam input
+            if (input_kata[:len(self.input_lama)] != self.input_lama or len(input_kata) < len(self.input_lama)): 
+                # Cari melalui dataset
+                for kata in self.dataset:
+                    if prefix.match(kata):
+                        list_rekomendasi.append(kata)
+
+            else:
+                # Cari melalui list_lama
+                for kata in self.list_lama:
+                    if prefix.match(kata):
+                        list_rekomendasi.append(kata)
+
+
+        # Masukkan hasil yang ada di dalam list_rekomendasi ke list_lama
         self.list_lama = list_rekomendasi
         # Ubah input_lama dengan input_kata saat ini
         self.input_lama = input_kata
-        # Masukkan hasil list_rekomendasi ke dalam cache untuk digunakan kembali
-        self.cache[input_kata] = list_rekomendasi;
+        # Masukkan hasil list_rekomendasi ke dalam history untuk digunakan kembali
+        self.history[input_kata] = list_rekomendasi;
 
         return list_rekomendasi
